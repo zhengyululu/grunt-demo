728748306d89d953b9128217f7d0b6f73110496b
zhengyululu <2637106664@qq.com>
2020-02-25 19:46:23 +0800

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,14 @@
 module.exports = function (grunt) {
     grunt.initConfig({
-      less: {
-        compile: {
-          files: { 'dist/compiled.css': 'css/*.less' }
-        } 
+      htmlhint: {
+        options: {
+          htmlhintrc: './.htmlhintrc'
+        } ,
+        src:['*.html']
       }
     });
   
-    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-htmlhint');
   
-    grunt.registerTask('default', ['less:compile']);
+    grunt.registerTask('default', ['htmlhint']);
   };
